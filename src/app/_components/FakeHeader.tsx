a33c96b99372f79777005c0ed3d313eb78c76778
Joe Farhaven <157184041+JoeFarhavenUSDS@users.noreply.github.com>
2024-12-31 17:06:21 +0000

    Fixing visual appearance of fake header

--- src/app/_components/FakeHeader.tsx
+++ src/app/_components/FakeHeader.tsx
@@ -4,7 +4,7 @@ const FakeHeader = () => (
   <header className="usa-header usa-header--extended" role="banner">
     <div style={{backgroundColor: '#222c67', padding: '10px', width: '100%'}}>
       <Image src="/site/joefarhavenusds/unified-intake-tool/FDA_Logo_Icon_White.svg" style={{height:80, paddingTop: '10px', paddingLeft: '10px'}} alt="FDA Logo" />
-      <ul style={{listStyleType: 'none', float: 'right', marginTop: '30px'}}>
+      <ul style={{listStyleType: 'none', float: 'right', marginTop: '-50px'}}>
         <li style={{display: 'inline-block'}}><a title="" id="btn-search" style={{borderColor: 'rgb(229,182,17)', borderRadius: '3px', borderStyle: 'solid', borderWidth: '1px', fontFamily: 'Helvetica, Arial', fontSize: '12px', fontWeight: '400', paddingBottom: '5px', paddingLeft: '10px', paddingTop: '5px', paddingRight: '10px', color: 'white', margin: '10px'}}>
           <span className="fa fa-search" aria-hidden="true">&nbsp;</span>
           <span>Search</span>
