18e0b7063eff7c698fe79ef2b55aa0ab0608edaf
Joe Farhaven <157184041+JoeFarhavenUSDS@users.noreply.github.com>
2024-12-31 00:44:32 +0000

    Tweaking spacing on ProcesList

--- src/app/_components/OverviewProcessList.tsx
+++ src/app/_components/OverviewProcessList.tsx
@@ -3,19 +3,19 @@ const OverviewProcessList = () => (
   <ul className="usa-process-list">
     <li className="usa-process-list__item">
       <h2 className="usa-process-list__heading font-ui-lg">The product</h2>
-      <p></p>
+      <p>&nbsp;</p>
     </li>
     <li className="usa-process-list__item">
       <h2 className="usa-process-list__heading font-ui-lg">What happened</h2>
-      <p></p>
+      <p>&nbsp;</p>
     </li>
     <li className="usa-process-list__item">
       <h2 className="usa-process-list__heading font-ui-lg">Who was harmed <span className="text-normal">(optional)</span></h2>
-      <p></p>
+      <p>&nbsp;</p>
     </li>
     <li className="usa-process-list__item">
       <h2 className="usa-process-list__heading font-ui-lg">How to reach you <span className="text-normal">(optional)</span></h2>
-      <p></p>
+      <p>&nbsp;</p>
     </li>
   </ul>
 );
