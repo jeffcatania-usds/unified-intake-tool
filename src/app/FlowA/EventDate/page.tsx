b6d0ab3ff10d1b1f6a26fc9d6e7c6f8008e0f770
Joe Farhaven <157184041+JoeFarhavenUSDS@users.noreply.github.com>
2025-01-07 19:01:38 +0000

    Improved date handling for Flow A

--- src/app/FlowA/EventDate/page.tsx
+++ src/app/FlowA/EventDate/page.tsx
@@ -13,21 +13,41 @@ import { useState } from "react";
 import NavigateBack from "@/_components/NavigateBack";
 import NavigateNext from "@/_components/NavigateNext";
 import { EventDateMetadata } from "./metadata";
+import { z } from "zod";
 
 export default function EventDate() {
   const screenName = EventDateMetadata.name;
   const { userData, updateUserData } = useUserDataContext();
   const [validated, setValidated] = useState(false);
+  const [month, setMonth] = useState("");
+  const [day, setDay] = useState("");
+  const [year, setYear] = useState("");
 
   const handleMonthChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    updateUserData(EVENT_DATE, event.target.value);
+    setMonth(event.target.value);
+    updateUserData(EVENT_DATE, formatDate());
   };
   const handleDayChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    updateUserData(EVENT_DATE, event.target.value);
+    setDay(event.target.value);
+    updateUserData(EVENT_DATE, formatDate());
   };
   const handleYearChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    updateUserData(EVENT_DATE, event.target.value);
+    setYear(event.target.value);
+    updateUserData(EVENT_DATE, formatDate());
   };
+
+  const formatDate = () => {
+    let result = "";
+    try {
+      result = new Date(month + "/" + day + "/" + year).toDateString();
+    } catch {
+      // If the date is invalid, default to a less specific value.
+      result = month ? month + "/" : "";
+      result += year;
+    }
+    return result;
+  };
+
   const validate = (event: React.ChangeEvent) => {
     if (!isValid()) {
       event.preventDefault();
@@ -37,9 +57,20 @@ export default function EventDate() {
     return true;
   };
 
+  // Require at minimum a 2-digit year.
+  const yearSchema = z.number().min(10);
+  const dateSchema = z.coerce.date();
+
   const isValid = () => {
-    return userData[EVENT_DATE];
+    // If a day is provided, ensure that the date is valid.
+    if (day.length > 0) {
+      return dateSchema.safeParse(formatDate());
+    } else {
+      // If a day is not provided, require a valid year.
+      return yearSchema.safeParse(year).success;
+    }
   };
+
   return (
     <>
       <NavigateBack userData={userData} screenName={screenName} />
@@ -74,6 +105,7 @@ export default function EventDate() {
             <Select
               id="eventDateMonth"
               name="eventDateMonth"
+              value={month}
               onChange={handleMonthChange}
             >
               <option>- Select -</option>
@@ -89,6 +121,7 @@ export default function EventDate() {
               <option value="10">10 - October</option>
               <option value="11">11 - November</option>
               <option value="12">12 - December</option>
+              <option>Not sure</option>
             </Select>
           </FormGroup>
           <DateInput
@@ -98,6 +131,7 @@ export default function EventDate() {
             unit="day"
             maxLength={2}
             placeholder="DD"
+            value={day}
             onChange={handleDayChange}
           />
           <DateInput
@@ -107,6 +141,7 @@ export default function EventDate() {
             unit="year"
             maxLength={4}
             placeholder="YYYY"
+            value={year}
             onChange={handleYearChange}
             required
           />
