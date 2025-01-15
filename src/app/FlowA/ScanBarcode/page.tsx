6e7261bfd51a6ce7b280ed91e5c2c389560ab6e7
Joe Farhaven <157184041+JoeFarhavenUSDS@users.noreply.github.com>
2025-01-15 21:31:22 +0000

    Removing reference to non-working UPC lookup

--- src/app/FlowA/ScanBarcode/page.tsx
+++ src/app/FlowA/ScanBarcode/page.tsx
@@ -7,7 +7,6 @@ import { ScanBarcodeMetadata } from "./metadata";
 import ScreenWithNavigation from "@/_components/ScreenWithNavigation";
 import Html5QrcodePlugin from "@/_components/Html5QrcodePlugin";
 import { useState } from "react";
-import UPCLookup from "@/_components/UPCLookup";
 
 export default function ScanBarcode() {
   const screenName = ScanBarcodeMetadata.name;
@@ -59,9 +58,6 @@ export default function ScanBarcode() {
             onChange={handleBarcodeTextChange}
             type="text"
           />
-          {userData[BARCODE] && typeof userData[BARCODE] === "string" && (
-            <UPCLookup upc={userData[BARCODE]} />
-          )}
         </FormGroup>
       )}
     </ScreenWithNavigation>
